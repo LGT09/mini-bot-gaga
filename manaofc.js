8e8e7db881cbac61669dba0df3c74c513b59d834
manaofc <manisha1718manisha@gmail.com>
2026-01-25 01:55:02 +0530

    Update manaofc.js

--- manaofc.js
+++ manaofc.js
@@ -80,7 +80,7 @@ if (process.env.GITHUB_TOKEN) {
     });
 }
 const owner = process.env.GITHUB_REPO_OWNER || "manaofc";
-const repo = process.env.GITHUB_REPO_NAME || "manaofc-minibot";
+const repo = process.env.GITHUB_REPO_NAME || "mini-bot";
 
 // Memory optimization: Use weak references for sockets
 const activeSockets = new Map();
