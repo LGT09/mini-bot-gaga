b6c1175208b9c7ed1a32f645e751dac7228da199
manaofc <manisha1718manisha@gmail.com>
2026-01-25 00:44:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
+
 const PORT = process.env.PORT || 8000;
-let code = require('./manaofc'); 
+const code = require('./manaofc');
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
+// ===== Middleware (MUST be before routes) =====
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ===== Routes =====
 app.use('/code', code);
-app.use('/bot', async (req, res, next) => {
-    res.sendFile(__path + '/manaofc.js')
-});
-app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/main.html')
+
+app.get('/bot', (req, res) => {
+    res.sendFile(path.join(__dirname, 'manaofc.js'));
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
 
+// ===== Server =====
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:` + PORT)
+    console.log(`Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
